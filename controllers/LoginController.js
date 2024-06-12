a148b3c3a018067a81ad7a7b8885187f30542e19
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-12 17:12:44 +0200

    fix(tokens insert to bdd)

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -17,15 +17,15 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials!" });
     }
     const token = jwt.sign({ id: user.user_id }, process.env.JWT_SECRET, {
-      expiresIn: "15m",
+      expiresIn: "1m",
     });
     const refreshToken = jwt.sign(
       { id: user.user_id },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "7d" }
+      { expiresIn: "1d" }
     );
-    user.refreshToken = refreshToken;
-    user.accessToken = token;
+    user.user_token_refresh = refreshToken;
+    user.user_token_access = token;
     await user.save();
     res.status(200).json({
       msg: "Connected!",
--- controllers/TokenRefreshController.js
+++ controllers/TokenRefreshController.js
@@ -8,18 +8,18 @@ module.exports = router;
 router.post("/", async (req, res) => {
   const invalidToken = req.body.token.split(" ")[1];
   try {
-    const user = await User.findOne({ where: { accessToken: invalidToken } });
+    const user = await User.findOne({ where: { user_token_access: invalidToken } });
     if (!user) {
       return res.status(444).json({ message: "User not found!" });
     }
-    if (!user.refreshToken) {
+    if (!user.user_token_refresh) {
       return res.status(444).json({ message: "No refresh token available!" });
     }
-    jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    jwt.verify(user.user_token_refresh, process.env.REFRESH_TOKEN_SECRET);
     const newAccessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "15m",
     });
-    user.accessToken = newAccessToken;
+    user.user_token_access = newAccessToken;
     await user.save();
     res.status(200).json({ message: "Token refreshed successfully!", newAccessToken });
   } catch (error) {
