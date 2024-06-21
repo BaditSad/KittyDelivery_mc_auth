d1d14258317a7e6e0f8881ffd8ab4f8a2c2d2a7f
kilyion <kilyion.romary@viacesi.fr>
2024-06-21 03:26:41 +0200

    login check token + user status

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -8,7 +8,6 @@ const router = express.Router();
 module.exports = router;
 
 router.post("/", 
-  
   [
     check('email').isEmail().withMessage('Email invalide'), 
     check('password').not().isEmpty().withMessage('Le mot de passe est requis') 
@@ -19,42 +18,46 @@ router.post("/",
       return res.status(400).json({ errors: errors.array() }); 
     }
   
-  try {
-    const user = await User.findOne({ where: { user_email: req.body.email } });
+    try {
+      const user = await User.findOne({ where: { user_email: req.body.email } });
 
-    if (!user) {
-      return res.status(404).json({ message: "Utilisateur non trouvé" });
+      if (!user) {
+        return res.status(404).json({ message: "Utilisateur non trouvé" });
+      }
+
+      if (user.user_account_status.toLowerCase() !== 'active') {
+        return res.status(403).json({ message: 'Compte utilisateur inactif' });
+      }
+
+      const isMatch = await argon2.verify(user.user_password, req.body.password);
+
+      if (!isMatch) {
+        return res.status(404).json({ message: "Mot de passe incorrect" });
+      }
+
+      const token = jwt.sign({ id: user.user_id }, process.env.JWT_SECRET, {
+        expiresIn: "15m",
+      });
+
+      const refreshToken = jwt.sign(
+        { id: user.user_id },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "16h" }
+      );
+
+      user.user_token_refresh = refreshToken;
+      user.user_token_access = token;
+
+      await user.save();
+
+      res.status(201).json({
+        id: user.user_id,
+        address: user.user_address,
+        role: user.user_role,
+        token: token,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-
-    const isMatch = await argon2.verify(user.user_password, req.body.password);
-
-    if (!isMatch) {
-      return res.status(404).json({ message: "Mot de passe incorrect" });
-    }
-
-    const token = jwt.sign({ id: user.user_id }, process.env.JWT_SECRET, {
-      expiresIn: "15m",
-    });
-
-    const refreshToken = jwt.sign(
-      { id: user.user_id },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "16h" }
-    );
-
-    user.user_token_refresh = refreshToken;
-    user.user_token_access = token;
-
-    await user.save();
-
-    //if user_role === restaurateur alors address = restaurant_address where user_id = user_id
-    res.status(201).json({
-      id: user.user_id,
-      address: user.user_address,
-      role: user.user_role,
-      token: token,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
   }
-});
+);
